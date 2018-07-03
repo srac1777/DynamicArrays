b17c4c3e718fb9c4e9681a0e1e251a1060b06927
Shashank Racherla <shashank.racherla@gmail.com>
2018-07-02 20:10:04 -0700

    finished static array

--- lib/static_array.rb
+++ lib/static_array.rb
@@ -1,14 +1,17 @@
 # This class just dumbs down a regular Array to be statically sized.
 class StaticArray
   def initialize(length)
+    @arr = Array.new(length)
   end
 
   # O(1)
   def [](index)
+    @arr[index]
   end
 
   # O(1)
   def []=(index, value)
+    @arr[index] = value
   end
 
   protected
