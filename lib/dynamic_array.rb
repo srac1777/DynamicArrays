4b5ab7d38d2e30592db9251a49de7de5d4dfbca5
Shashank Racherla <shashank.racherla@gmail.com>
2018-07-24 14:09:40 -0700

    dy2

--- lib/dynamic_array.rb
+++ lib/dynamic_array.rb
@@ -3,30 +3,42 @@ require 'byebug'
 
 class DynamicArray
   attr_reader :length
+  attr_accessor :capacity, :store
 
   def initialize
-    
+    @store = StaticArray.new(8)
+    @length = 0
+    @capacity = 8
   end
 
   # O(1)
   def [](index)
-    
+    # debugger
+    raise "index out of bounds" unless check_index(index)
+    @store[index]
   end
 
   # O(1)
   def []=(index, value)
-    
+    raise "index out of bounds" unless check_index(index)
+    @store[index] = value
   end
 
   # O(1)
   def pop
-    
+    toreturn = @store[length]
+    @length -= 1
+    toreturn
   end
 
   # O(1) ammortized; O(n) worst case. Variable because of the possible
   # resize.
   def push(val)
-    
+    if length == capacity
+      resize!
+    end
+    @length += 1
+    @store[length] = val
   end
 
   # O(n): has to shift over all the elements.
@@ -44,10 +56,21 @@ class DynamicArray
   attr_writer :length
 
   def check_index(index)
+    return false if length == 0
+    return false if index >= capacity
+    return false if index < 0
+    true
   end
 
   # O(n): has to copy over all the elements to the new store.
   def resize!
-    
+    oldstore = @store
+    @capacity *= 2
+    @store = StaticArray.new(@capacity)
+    i = 0
+    oldstore.each do |el|
+      @store[i] = el
+      i += 1
+    end
   end
 end
