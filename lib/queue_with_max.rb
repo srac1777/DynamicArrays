5889312913fd9a4fdf601c9620caba78ebfdae6b
srac1777 <shashank.racherla@gmail.com>
2018-09-03 20:07:11 -0700

    Dynamic Array Complete

--- lib/queue_with_max.rb
+++ lib/queue_with_max.rb
@@ -7,23 +7,59 @@
 # methods you need.
 
 require_relative 'ring_buffer'
+require 'byebug'
+
+# [5, 3, 2, 7, 6, 5, 4, 3, 2, 1, 8]
+# max = 7
+
+# maxArray = [5].
+# if el < maxArray { 
+#   while el > maxArray.last {
+#     maxArray.pop
+#   }
+#   maxArray.push(el)
+# }
 
 class QueueWithMax
   attr_accessor :store
 
   def initialize
+    @store = RingBuffer.new
+    @maxArray = []
+    @max = nil
   end
 
   def enqueue(val)
+    @store.push(val)
+    @max = val if @max.nil? || val > @max
+    if @maxArray.empty?
+      @maxArray << val 
+    else
+      if val < @max  
+          while @maxArray.length > 0 && val > @maxArray.last 
+                @maxArray.pop
+          end
+          @maxArray.push(val)
+      end
+    end
+
   end
 
   def dequeue
+    el = @store.shift
+    if el == @max
+      @maxArray.shift
+      @max = @maxArray[0]
+    end
+    el
   end
 
   def max
+    @max
   end
 
   def length
+    @store.length
   end
 
 end
--- lib/ring_buffer.rb
+++ lib/ring_buffer.rb
@@ -14,13 +14,13 @@ class RingBuffer
   # O(1)
   def [](index)
     raise "index out of bounds" unless check_index(index)
-    @store[index]
+    @store[(@start_idx + index) % @capacity]
   end
 
   # O(1)
   def []=(index, val)
     raise "index out of bounds" unless check_index(index)
-    @store[index] = val
+    @store[(@start_idx + index) % @capacity] = val
   end
 
   # O(1)
@@ -42,12 +42,22 @@ class RingBuffer
   def shift
     raise "index out of bounds" if @length == 0
     to_return = @store[@start_idx]
-    
+    @start_idx += 1
+    @length -= 1
+    to_return
   end
 
   # O(1) ammortized
   def unshift(val)
-
+    resize! if @length == @capacity
+    if @length == 0
+      @store[0] = val
+      @length += 1
+    else
+      @store[(@start_idx-1) % @capacity] = val
+      @start_idx -= 1
+      @length += 1
+    end
   end
 
   protected
@@ -62,5 +72,14 @@ class RingBuffer
   end
 
   def resize!
+    oldstore = @store.dup
+    @store = StaticArray.new(@capacity * 2)
+    i = 0
+    while i < @length
+      @store[i] = oldstore[(@start_idx+i) % capacity]
+      i+=1
+    end
+    @capacity *= 2
+    @start_idx = 0
   end
 end
